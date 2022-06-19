63d5263b0282aeee5560492bcec66112a77bc7cc
Nikita <traed.traed@mail.ru>
2022-06-19 14:49:19 +0300

    episode-2

---
+++ ts-basics/functions-test.ts
@@ -0,0 +1,6 @@
+import addNumbers, { addStrings, getName } from './functions';
+
+console.log(addNumbers(1, 2));
+console.log(addStrings('Hello'));
+
+console.log(getName({ first: 'Nikita', last: 'Sukhachev' }));
