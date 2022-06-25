06618ce2e21e6a017110f30793cb19bd74986793
Nikita <traed.traed@mail.ru>
2022-06-25 18:55:14 +0300

    episode-10

---
+++ ts-basics/readonly-1.ts
@@ -0,0 +1,26 @@
+interface Cat {
+  name: string;
+  breed: string;
+}
+
+function makeCat(name: string, breed: string): Readonly<Cat> {
+  return {
+    name,
+    breed,
+  };
+}
+
+const usul = makeCat('Usul', 'Tabby');
+console.log(usul);
+
+function makeCoordinate(
+  x: number,
+  y: number,
+  z: number
+): readonly [number, number, number] {
+  return [x, y, z];
+}
+
+const c1 = makeCoordinate(10, 20, 30);
+
+const reallyConst = [1, 2, 3] as const;
